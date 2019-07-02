809f6ae2c03b064e863ed0df46d836da200df500
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-07-02 10:39:18 +0300

    Add SILVER license level

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,6 +5,7 @@ use DreamFactory\Core\MySqlDb\Database\Schema\MySqlSchema;
 use DreamFactory\Core\MySqlDb\Models\MySqlDbConfig;
 use DreamFactory\Core\MySqlDb\Services\MySqlDb;
 use DreamFactory\Core\Components\DbSchemaExtensions;
+use DreamFactory\Core\Enums\LicenseLevel;
 use DreamFactory\Core\Services\ServiceManager;
 use DreamFactory\Core\Services\ServiceType;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
@@ -24,12 +25,13 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'            => 'mysql',
-                    'label'           => 'MySQL',
-                    'description'     => 'Database service supporting MySQL connections.',
-                    'group'           => ServiceTypeGroups::DATABASE,
-                    'config_handler'  => MySqlDbConfig::class,
-                    'factory'         => function ($config) {
+                    'name'                  => 'mysql',
+                    'label'                 => 'MySQL',
+                    'description'           => 'Database service supporting MySQL connections.',
+                    'group'                 => ServiceTypeGroups::DATABASE,
+                    'config_handler'        => MySqlDbConfig::class,
+                    'subscription_required' => LicenseLevel::SILVER,
+                    'factory'               => function ($config) {
                         return new MySqlDb($config);
                     },
                 ])
